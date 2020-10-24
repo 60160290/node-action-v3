8ef6490ea76ec5b11d6500330740fbd8cf3157c8
60160290 <60160290@go.buu.ac.th>
2020-10-24 15:27:33 +0700

    add test GET '/'

--- tests/test.js
+++ tests/test.js
@@ -1,7 +1,13 @@
 const assert = require('assert')
+const { request } = require('https')
 
 describe('Validate setting the test', () => {
     it('should be ok', () => {
         assert.ok(false)
     })
+describe('GET "/"', () =>{
+    it('respond with hello!', (done) =>{
+        request(app).getHeader('/'.expect('Hello!', done))
+    })
+})
 })
