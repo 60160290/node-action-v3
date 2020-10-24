a361bafcb60555e027c096fda94c8b6754faeab1
60160290 <60160290@go.buu.ac.th>
2020-10-24 15:47:19 +0700

    add test GET '/'

--- index.js
+++ index.js
@@ -2,6 +2,20 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
 
+let phones = [
+    { id: 1, name:'iPhone12 Pro Max'},
+    { id: 2, name:'Google Pixel 5'}
+  ]
+
+app.get('/list', (req, res) => {
+    res.json(phones)
+})
+
+app.get('/:id', (req, res) => {
+    let id = req.params.id
+    res.json(phones[id-1])
+})
+
 app.get('/', (req, res) => {
     return res.send('Hello!')
 })
@@ -11,3 +25,10 @@ app.listen(port, () => {
 })
 
 module.exports = app
+
+// Exercise
+// GET /items
+// [
+//     { id: 1. name:'iPhone12 Pro Max'}
+//     { id: 2. name:'Google Pixel 5'}
+// ]
