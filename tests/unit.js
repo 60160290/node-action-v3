57ccdf54b2624dd0932298d15eb43f22b972faac
60160290 <60160290@go.buu.ac.th>
2020-11-06 22:02:12 +0700

    fix tests

--- tests/unit.js
+++ tests/unit.js
@@ -10,9 +10,9 @@ describe('GET "/"', () =>{
 describe('GET "/items"', () =>{
     it('respond with hello!', (done) =>{
         let want = [
-        { id : 1, name:'iPhone12 Pro Max'},
-        { id: 2, name:'Google Pixel 5'}
+            { id : 1, name:'iPhone12 Pro Max'},
+            { id: 2, name:'Google Pixel 5'}
         ]
         request(app).get('/items').expect(want, done)
         })
-    })
+})
